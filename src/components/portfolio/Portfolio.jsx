fd7e30ae67bf782cebc4a29274b363beecd78c06
chandana <srichandanach09@gmail.com>
2023-06-17 18:39:18 -0400

    updated links in portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -18,7 +18,7 @@ const Portfolio = () => {
           <h3>Crimson Board - The Learning Management System</h3>
           <div className="actionbtn">
           <a href='https://github.com/Chakilamchandana/django_lms' className='btn' target='_blank'>GitHub</a>
-          <a href='https://github.com/Chakilamchandana/django_lms/tree/main/demo' className='btn btn-primary' target='_blank'>Demo</a>
+          <a href='https://drive.google.com/file/d/1MDKJ6Sfl7JpR_6RZt51TwprtZrkYlFaP/view?usp=drive_link' className='btn btn-primary' target='_blank'>Demo</a>
           </div>
         </article>
 
@@ -29,7 +29,7 @@ const Portfolio = () => {
           <h3>Life Expectancy Prediction Model </h3>
           <div className="actionbtn">
             <a href='https://github.com/Chakilamchandana/Life-Expectancy-Prediction/tree/main/project-schakil-ltaduru-main' className='btn' target='_blank'>GitHub</a>
-            <a href='https://github.com/Chakilamchandana/Life-Expectancy-Prediction/blob/main/project-schakil-ltaduru-main/VID-20221204-WA0037.mp4' className='btn btn-primary' target='_blank'>Demo</a>
+            <a href='https://docs.google.com/presentation/d/e/2PACX-1vQUO1IbdSrAyBY0P3Zcbt6p0ZSnwsgmN1bUpkSaOV1HpcY7rd_0h0HYAG5UHvyHlw/pub?start=false&loop=false&delayms=5000' className='btn btn-primary' target='_blank'>Demo</a>
           </div>
         </article>
 
